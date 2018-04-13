2371607080872dbb8d80d819e7cd52fcad577758
John Kim <john.kim@appian.com>
2018-04-13 17:22:56 -0400

    More WIP

--- crHandler.js
+++ crHandler.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const octokit = require('@octokit/rest')({ debug: true });
 const request = require('request');
+const crypto = require('crypto');
 
-const GITHUB_ICON = 'https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png';
+const GITHUB_ICON = 'https://www.iconsdb.com/icons/preview/caribbean-blue/github-6-xxl.png';
 
 const squadToChannel = {
-  '@organization/team-1'         : 'mattermost-channel-1',
-  '@organization/team-1'         : 'mattermost-channel-2',
+  '@test-squad1': 'johns-private-channel',
+  '@test-squad2': 'johns-private-channe2',
 }
 
 module.exports = {
@@ -14,31 +15,45 @@ module.exports = {
 }
 
 function getCRs(req, res) {
+  let calculatedSignature = 'sha1=' + crypto.createHmac("sha1", process.env.GITHUB_WEBHOOK_SECRET).update(JSON.stringify(req.body)).digest('hex');
+  if (req.headers['x-hub-signature'] != calculatedSignature) {
+    return res.json({ error: 'Incorrect signature' });
+  }
+
+  let prTitle = req.body.issue.title;
   let prURL = req.body.issue.html_url;
   let prComment = req.body.comment.body;
   let prCommenter = req.body.comment.user.login;
 
   if (req.body.action == 'created') {
     let squads = prComment.match(/@(\w|-|_)+/g);
+
     squads.forEach(squad => {
       if (squad in squadToChannel) {
         let mattermostChannel = squadToChannel[squad];
-        let mattermostComment = '@here ' + prURL + '\nCommented by: ' + prCommenter + '\n\nPull request comment:\n' + prComment;
-
-        request.post({
-          json: true,
-          url: process.env.MATTERMOST_URL,
-          headers: { 'Content-Type': 'application/json'},
-          body: {
-            username: 'GitHub Bot',
-            channel: mattermostChannel,
-            text: mattermostComment,
-            icon_url: GITHUB_ICON
-          },
-        }, (error, response, body) => {
-          console.log(body);
-        });
+        let mattermostComment = `@here\n` +
+                                `### [${prTitle}](${prURL})\n` +
+                                `Commented by: ${prCommenter}\n` +
+                                `PR comment:\n` +
+                                `${prComment}`;
+        postToMattermost(mattermostChannel, mattermostComment);
       }
     });
   }
-};
+}
+
+function postToMattermost(channel, comment) {
+  request.post({
+    json: true,
+    url: process.env.MATTERMOST_URL,
+    headers: { 'Content-Type': 'application/json' },
+    body: {
+      username: 'GitHub Bot',
+      channel: channel,
+      text: comment,
+      icon_url: GITHUB_ICON
+    },
+  }, (error, response, body) => {
+    console.log(body);
+  });
+}
--- prHandler.js
+++ prHandler.js
@@ -1,42 +1,48 @@
 require('dotenv').config();
 const octokit = require('@octokit/rest')({ debug: true });
 
-const GITHUB_ICON = 'https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png';
+const GITHUB_ICON = 'https://www.iconsdb.com/icons/preview/caribbean-blue/github-6-xxl.png';
 
 module.exports = {
   getPRs: getPRs
 }
 
 function getPRs(req, res) {
-  if (req.body.token != process.env.MATTERMOST_TOKEN)
+  if (req.body.token != process.env.MATTERMOST_TOKEN) {
     return res.json({ error: 'Incorrect token' });
+  }
 
-  let parameters = req.body.text.split(" ");
+  let parameters = req.body.text.split(' ');
+  if (parameters.length != 2) {
+    let text = 'Incorrect number of arguments :(';
+    return mattermostResponse(res, text);
+  }
+
+  const repoOwner = 'appian';
   let githubUsername = parameters[0];
   let repo = parameters[1];
-  const repoOwner = 'appian';
 
-  let text = 'PRs created by **' + githubUsername + '** in **' + repo + '**:\n';
+  octokit.issues.getForRepo({ owner: repoOwner, repo: repo, creator: githubUsername }).then(result => {
+    let text = `### PRs created by ${githubUsername} in ${repo}:\n`;
 
-  octokit.issues.getForRepo({owner:repoOwner, repo:repo, creator:githubUsername}).then(result => {
     result.data.forEach(prResponse => {
       let prTitle = prResponse.title;
       let prURL = prResponse.html_url;
-      text += '\n' + prTitle + ': ' + prURL;
+      text += `${prTitle}: ${prURL}\n`;
     });
+
+    return mattermostResponse(res, text);
   }).catch(error => {
+    let inputCommand = `/prs ${githubUsername} ${repo}\n`;
+    let errorMessage = ''
+
     if (error.code == '404') {
-      text = '404 error. Did you provide the correct GitHub username and repo?';
+      errorMessage = '404 error. Did you provide the correct GitHub username and repo?';
     } else {
-      text = 'Unknown error.';
+      errorMessage = 'Unknown error.';
     }
-  }).then(() => {
-    return res.json({
-      username: 'GitHub Bot',
-      response_type: 'ephemeral',
-      text: text,
-      icon_url: GITHUB_ICON
-    });
+
+    return mattermostResponse(res, inputCommand + errorMessage);
   });
 };
 
@@ -44,3 +50,12 @@ octokit.authenticate({
   type: 'token',
   token: process.env.GITHUB_TOKEN
 });
+
+function mattermostResponse(res, text) {
+  return res.json({
+    username: 'GitHub Bot',
+    response_type: 'ephemeral',
+    text: text,
+    icon_url: GITHUB_ICON
+  });
+}
